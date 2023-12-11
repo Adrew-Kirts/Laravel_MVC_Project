ba67a185663da81a72880504d0fa81bcf704d76b
Ezra Strikwerda <ezra@strikwerda.fr>
2023-12-11 15:08:05 +0100

    Installed tailwind

--- resources/views/movies-list.blade.php
+++ resources/views/movies-list.blade.php
@@ -1,5 +1,13 @@
-<html>
+<html lang="en">
+<head>
+    <title>Movies List</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    @vite('resources/css/app.css')
+</head>
 <body>
-<h1>All Movies</h1>
+<h2 class="text-3xl font-bold underline">
+    All movies
+</h2>
 </body>
 </html>
